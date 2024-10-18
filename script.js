33e9fa3eea6184c9594bcf72b9610fd17727737a
Shreya chincholkar <chincholkarshreya@gmail.com>
2024-10-18 09:23:14 +0530

    added more sections

--- script.js
+++ script.js
@@ -2,23 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
   const expandButtons = document.querySelectorAll('.expand-btn');
   const closeButtons = document.querySelectorAll('.close-btn');
 
-  
-  // Event listener to open corresponding expand div
   expandButtons.forEach(button => {
     button.addEventListener('click', function () {
       const target = button.getAttribute('data-target');
       const expandDiv = document.getElementById(target);
       const gif = expandDiv.querySelector('.expandGIF img');
       
-      // Reset GIF by re-assigning its src attribute
       const gifSrc = gif.getAttribute('src');
-      gif.setAttribute('src', gifSrc); // Reassigning the same src
-
+      gif.setAttribute('src', gifSrc); 
       expandDiv.classList.add('show');
     });
   });
 
-  // Event listener to close expand div
+
   closeButtons.forEach(button => {
     button.addEventListener('click', function () {
       const expandDiv = button.closest('.expand');
